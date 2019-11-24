41556752b5d5a5a93e6ec7205a347aa3e023b859
LeoDog896 <LeoDog896@hotmail.com>
2019-11-24 13:45:31 -0500

    Added ElytraBoost

--- cepi-gameplay/src/me/cepi/gameplay/Main.java
+++ cepi-gameplay/src/me/cepi/gameplay/Main.java
@@ -1,13 +1,23 @@
 package me.cepi.gameplay;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.cepi.gameplay.modules.AntiCropTrample;
-
+import me.cepi.gameplay.modules.ElytraBoost;
 
 public class Main extends JavaPlugin {
+	
+	Logger logger = Logger.getLogger("Minecraft");
 
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new AntiCropTrample(), this);
+        getServer().getPluginManager().registerEvents(new ElytraBoost(), this);
+        this.logger.info("[Cepi Gameplay] has been Enabledd.");
     }
+    
+	public void onDisable() {
+		this.logger.info("[Cepi Gameplay] has been Disabledd.");
+	}
 }
---
+++ cepi-gameplay/src/me/cepi/gameplay/modules/ElytraBoost.java
@@ -0,0 +1,20 @@
+package me.cepi.gameplay.modules;
+
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+
+public class ElytraBoost implements Listener {
+	
+	@EventHandler
+	public void onPlayerToggleSneakEvent(PlayerToggleSneakEvent event) {
+		Player player = event.getPlayer();
+		if (player.isSneaking() && player.getInventory().getChestplate().getType() == Material.ELYTRA) {
+			player.setVelocity(player.getLocation().getDirection().multiply(2));
+			player.playSound(player.getLocation(), Sound.ENTITY_ENDER_DRAGON_FLAP, 1, 1);
+		}
+	}
+}
