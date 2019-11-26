8f3a1259c7897a0b9be1d594c07bb64b5598a7db
Jeremy Jacob <jeremjacob5705@student.lvusd.org>
2019-11-25 16:43:46 -0800

    Create Inserts.java

---
+++ cepi-gameplay/src/main/java/me/cepi/gameplay/modules/Inserts.java
@@ -0,0 +1,34 @@
+package me.cepi.gameplay.modules;
+
+import org.bukkit.ChatColor;
+
+public final class Inserts {
+    public static final String POSITIVE = ChatColor.DARK_GRAY
+                                          + "["
+                                          + ChatColor.DARK_GREEN
+                                          + "-"
+                                          + ChatColor.DARK_GRAY
+                                          + "] "
+                                          + ChatColor.GRAY;
+    public static final String NEGATIVE = ChatColor.DARK_GRAY
+                                          + "["
+                                          + ChatColor.DARK_RED
+                                          + "-"
+                                          + ChatColor.DARK_GRAY
+                                          + "] "
+                                          + ChatColor.GRAY;
+    public static final String NEUTRAL = ChatColor.DARK_GRAY
+                                          + "["
+                                          + ChatColor.GOLD
+                                          + "*"
+                                          + ChatColor.DARK_GRAY
+                                          + "] "
+                                          + ChatColor.GRAY;
+    public static final String UP = ChatColor.DARK_GRAY
+                                          + "["
+                                          + ChatColor.YELLOW
+                                          + "^"
+                                          + ChatColor.DARK_GRAY
+                                          + "] "
+                                          + ChatColor.GRAY;
+}
